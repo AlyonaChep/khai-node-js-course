a32d7aae806a1d611af063a4579173562efe3bb0
AlyonaChep <alyona.chepelevich@gmail.com>
2024-11-14 20:38:27 +0200

    Add middleware, error handling

--- product.routes.js
+++ product.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const products = require('./products');
+const { blockSpecialBrand } = require('./middleware');
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.get('/products', (request, response) => {
 });
 
 // handle get request for path /products/:brand
-router.get('/products/:brand', (request, response) => {
+router.get('/products/:brand', blockSpecialBrand, (request, response) => {
 	const { brand } = request.params; // Access the brand parameter from the URL
 
 	// Filter products based on the brand parameter
@@ -17,5 +18,12 @@ router.get('/products/:brand', (request, response) => {
 
 	response.json(filteredProducts); // Send the filtered products as a JSON response
 });
+
+router.get('/productswitherror', (request, response) => {
+	let err = new Error("processing error ")
+	err.statusCode = 400
+	throw err
+});
+
 
 module.exports = router;
